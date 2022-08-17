84a0ed87661d88ddf3beed9a4738aaff2947f4a2
new_user <nrustgen@gmail.com>
2022-08-16 17:18:57 -0700

    3. Архитектура проекта - Аргегатор Java Вакансий [#260359]

--- src/main/java/ru/job4j/grabber/Parse.java
+++ src/main/java/ru/job4j/grabber/Parse.java
@@ -1,8 +1,7 @@
 package ru.job4j.grabber;
 
-import java.io.IOException;
 import java.util.List;
 
 public interface Parse {
-    List<Post> list(String link) throws IOException;
+    List<Post> list(String link);
 }
