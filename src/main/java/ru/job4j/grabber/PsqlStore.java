442675221bb278d1f14214538428b06f7343a4dc
new_user <nrustgen@gmail.com>
2022-08-18 15:11:49 -0700

    5. PsqlStore [#285209 #333601]

--- src/main/java/ru/job4j/grabber/PsqlStore.java
+++ src/main/java/ru/job4j/grabber/PsqlStore.java
@@ -3,6 +3,7 @@ package ru.job4j.grabber;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
@@ -32,7 +33,8 @@ public class PsqlStore implements Store, AutoCloseable {
     @Override
     public void save(Post post) {
         try (PreparedStatement statement =
-                cnn.prepareStatement("insert into post (name, text, link, created) values (?, ?, ?, ?)",
+                cnn.prepareStatement("insert into post (name, text, link, created) values (?, ?, ?, ?)"
+                               + "ON CONFLICT (link) DO NOTHING",
                         Statement.RETURN_GENERATED_KEYS)) {
             statement.setString(1, post.getTitle());
             statement.setString(2, post.getLink());
@@ -103,10 +105,19 @@ public class PsqlStore implements Store, AutoCloseable {
         Properties config = new Properties();
         try (InputStream in = PsqlStore.class.getClassLoader().getResourceAsStream("post.properties")) {
             config.load(in);
-            PsqlStore store = new PsqlStore(config);
         } catch (IOException e) {
             e.printStackTrace();
             throw new IllegalStateException(e);
+        }
+        try (PsqlStore store = new PsqlStore(config)) {
+            store.save(new Post("google", "https://www.google.com/",
+                    "Web searcher", LocalDateTime.now()));
+            store.save(new Post("duckduckgo", "https://duckduckgo.com/",
+                    "Web searcher", LocalDateTime.now()));
+            System.out.println(store.findById(2));
+            store.getAll().forEach(System.out::println);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
