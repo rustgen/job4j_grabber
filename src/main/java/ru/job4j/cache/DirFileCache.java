c2309cdc397669f72ecd4de2c49d440dcca3ac89
new_user <nrustgen@gmail.com>
2022-09-02 13:54:30 -0700

    1. Реализация кеша на SoftReference [#1592 #337816]

--- src/main/java/ru/job4j/cache/DirFileCache.java
+++ src/main/java/ru/job4j/cache/DirFileCache.java
@@ -16,17 +16,10 @@ public class DirFileCache extends AbstractCache<String, String> {
 
     @Override
     protected String load(String key) {
-        String res = "";
+        String res;
         try {
             res = Files.readString(Path.of(cachingDir, key));
         } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try (BufferedReader in = new BufferedReader(
-                new FileReader(res))) {
-            res = in.lines().collect(Collectors.joining(System.lineSeparator()));
-        } catch (IOException e) {
-            e.printStackTrace();
             throw new IllegalArgumentException(e);
         }
         return res;
