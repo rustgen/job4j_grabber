51e83b04ad0ce67c03c34267c0f7a9347c8d3664
new_user <nrustgen@gmail.com>
2022-09-04 16:00:54 -0700

    2. Что такое TDD? [#4918 #339084]

--- src/test/java/ru/job4j/tdd/CinemaTest.java
+++ src/test/java/ru/job4j/tdd/CinemaTest.java
@@ -1,66 +1,63 @@
-//package ru.job4j.tdd;
-//
-//import org.junit.Ignore;
-//import org.junit.jupiter.api.Disabled;
-//import org.junit.jupiter.api.Test;
-//import static org.assertj.core.api.Assertions.*;
-//import static org.junit.jupiter.api.Assertions.assertThrows;
-//
-//import java.util.Calendar;
-//import java.util.List;
-//
-//@Disabled
-//public class CinemaTest {
-//
-//    @Ignore
-//    @Test
-//    public void whenBuy() {
-//        Account account = new AccountCinema();
-//        Cinema cinema = new Cinema3D();
-//        Calendar date = Calendar.getInstance();
-//        Ticket ticket = cinema.buy(account, 1, 1, date);
-//        assertThat(ticket).isEqualTo(new Ticket3D());
-//    }
-//
-//    @Ignore
-//    @Test
-//    public void whenFind() {
-//        Cinema cinema = new Cinema3D();
-//        cinema.add(new Session3D());
-//        List<Session> sessions = cinema.find(session -> true);
-//        assertThat(sessions).isNull();
-//    }
-//
-//    @Ignore
-//    @Test()
-//    public void whenInvalidPlace() {
-//        Account account = new AccountCinema();
-//        Cinema cinema = new Cinema3D();
-//        Calendar date = Calendar.getInstance();
-//        assertThrows(IllegalArgumentException.class, () -> {
-//            cinema.buy(account, -1, 1, date);
-//        });
-//    }
-//
-//    @Ignore
-//    @Test()
-//    public void whenInvalidDate() {
-//        Account account = new AccountCinema();
-//        Cinema cinema = new Cinema3D();
-//        Calendar date = Calendar.getInstance();
-//        date.set(1999, 12, 23);
-//        assertThrows(IllegalArgumentException.class, () -> {
-//            cinema.buy(account, 2, 1, date);
-//        });
-//    }
-//
-//    @Ignore
-//    @Test (expected = IllegalArgumentException.class)
-//    public void whenCantBuy() {
-//        Account account = new AccountCinema();
-//        Cinema cinema = new Cinema3D();
-//        Calendar date = Calendar.getInstance();
-//        Ticket ticket = cinema.buy(account, 1, 1, date);
-//        boolean expected = ticket.equals(new Ticket3D());
-//    }
-//}
+package ru.job4j.tdd;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.Calendar;
+import java.util.List;
+
+@Disabled
+public class CinemaTest {
+
+    @Test
+    public void whenBuy() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+        assertThat(ticket).isEqualTo(new Ticket3D());
+    }
+
+    @Test
+    public void whenFind() {
+        Cinema cinema = new Cinema3D();
+        cinema.add(new Session3D());
+        List<Session> sessions = cinema.find(session -> true);
+        assertThat(sessions).isNull();
+    }
+
+    @Test()
+    public void whenInvalidPlace() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        assertThrows(IllegalArgumentException.class, () -> {
+            cinema.buy(account, -1, 1, date);
+        });
+    }
+
+    @Test()
+    public void whenInvalidDate() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(1999, 12, 23);
+        assertThrows(IllegalArgumentException.class, () -> {
+            cinema.buy(account, 2, 1, date);
+        });
+    }
+
+    @Test
+    public void whenCantBuy() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        Ticket ticket1 = cinema.buy(account, 1, 1, date);
+        Ticket ticket2 = cinema.buy(account, 1, 1, date);
+        assertThrows(IllegalArgumentException.class, () -> {
+            cinema.buy(account, 1, 1, date);
+        });
+    }
+}
