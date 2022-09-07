8f60981f2ac5eadd554be6c7d592a54045edc812
new_user <nrustgen@gmail.com>
2022-09-07 16:50:51 -0700

    0. Принцип открытости закрытости [#4914 #340067]

--- src/main/java/ru/job4j/ood/ocp/Vehicle.java
+++ src/main/java/ru/job4j/ood/ocp/Vehicle.java
@@ -1,6 +1,33 @@
 package ru.job4j.ood.ocp;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Vehicle {
+
+    private ArrayList<Vehicle> vehicles;
+    private String name;
+
+    public Vehicle(ArrayList<Vehicle> vehicles) {
+        this.vehicles = vehicles;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Vehicle> getVehicles() {
+        return vehicles;
+    }
+
+    public void setVehicles(ArrayList<Vehicle> vehicles) {
+        this.vehicles = vehicles;
+    }
 
     private static class Plane {
         public String move() {
@@ -12,5 +39,18 @@ public class Vehicle {
         public String move() {
             return "sail";
         }
+    }
+
+    public ArrayList<Vehicle> version(String year) {
+        vehicles.forEach(v -> System.out.println(v.getName() + year));
+        return vehicles;
+    }
+
+    public List<String> versionLast(ArrayList<Vehicle> list) {
+        List<String> versions = list.stream()
+                .map(Vehicle::getName)
+                .filter(v -> v.contains("2023") || v.contains("2024"))
+                .collect(Collectors.toList());
+        return versions;
     }
 }
